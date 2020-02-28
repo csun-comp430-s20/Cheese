d5b2e7fb481ac66f8b54a04827c914a4c5ea629c
Sangat Shah <sangatshah1@gmail.com>
2020-02-27 18:50:17 -0800

    a

---
+++ tokenizer.rkt
@@ -0,0 +1,104 @@
+#lang racket
+
+(define in (open-input-file "test.txt"))
+
+(define (fill_text file txt)
+  (let ([character (read-char file)])
+     (if (not (eof-object? character)) (fill_text file (append txt (list character))) txt)))
+   
+(define (text file) (fill_text file '()))
+
+
+(define (item txt pos) (char->integer (list-ref txt pos)))
+(define (to_string n) (string (integer->char n)))
+(define (concat val x) (string-append val (to_string x)))
+
+(define (is_int n) (and (> n 47) (< n 58)))
+(define (is_letter s) (or (and (> s 64) (< s 91)) (and (> s 96) (< s 123))))
+(define (is_bool b) (or (string=? b "True") (string=? b "False")))
+(define (is_quotation q) (= q 34))
+(define (is_leftparen p) (= p 40))
+(define (is_rightparen p) (= p 41))
+(define (is_while w) (string=? w "while"))
+(define (is_if i) (string=? i "if"))
+(define (is_else e) (string=? e "else"))
+
+(define (an_int digits) (string-append "Integer_token(" digits ")"))
+(define (a_string s) (string-append "String_token(" s ")"))
+(define (a_bool b) (string-append "Boolean_token(" b ")"))
+
+(define (a_token t) (not (void? t)))
+
+(define (Integer_Token txt pos val tokens)
+  (let ([num (item txt pos)])
+    (if (is_int num)
+        (Integer_Token txt (+ pos 1) (concat val num) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list (an_int val))))))))
+
+(define (String_Token txt pos val tokens)
+  (let ([character (item txt pos)])
+    (if (is_letter character)
+        (String_Token txt (+ pos 1) (concat val character) tokens)
+        (when (> (string-length val) 0)
+          (cond
+            [(is_bool val)  (tokenizer txt pos (length txt) (append tokens (list (a_bool val))))]
+            [else (tokenizer txt pos (length txt) (append tokens (list (a_string val))))])))))
+
+(define (Quotation_Token txt pos val tokens)
+  (let ([q (item txt pos)])
+    (if (is_quotation q)
+        (Quotation_Token txt (+ pos 1) (concat val q) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "Quotation_token")))))))
+
+(define (LeftParen_Token txt pos val tokens)
+  (let ([p (item txt pos)])
+    (if (is_leftparen p)
+        (LeftParen_Token txt (+ pos 1) (concat val p) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "LeftParen_token")))))))
+
+(define (RightParen_Token txt pos val tokens)
+  (let ([p (item txt pos)])
+    (if (is_rightparen p)
+        (RightParen_Token txt (+ pos 1) (concat val p) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "RightParen_token")))))))
+
+(define (While_Token txt pos val tokens)
+  (let ([w (item txt pos)])
+    (if (is_while w)
+        (While_Token txt (+ pos 1) (concat val w) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "While_token")))))))
+
+(define (if_Token txt pos val tokens)
+  (let ([i (item txt pos)])
+    (if (is_if i)
+        (While_Token txt (+ pos 1) (concat val i) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "if_token")))))))
+
+(define (else_Token txt pos val tokens)
+  (let ([e (item txt pos)])
+    (if (is_else e)
+        (While_Token txt (+ pos 1) (concat val e) tokens)
+        (when (> (string-length val) 0)
+          (tokenizer txt pos (length txt) (append tokens (list "else_token")))))))
+
+(define (tokenizer txt pos end tokens)
+  (if (< pos end)
+      (let ([c (item txt pos)])
+        (cond
+          [(or (= c 10) (= c 32)) (tokenizer txt (+ pos 1) end tokens)]
+          [(is_leftparen c) (LeftParen_Token txt pos "" tokens)]
+          [(is_rightparen c) (RightParen_Token txt pos "" tokens)]
+          [(is_int c) (Integer_Token txt pos "" tokens)]
+          [(is_quotation c) (Quotation_Token txt pos "" tokens)]
+          [else (String_Token txt pos "" tokens)]))
+      tokens))
+
+(let ([characters (text in)])
+  (tokenizer characters 0 (length characters) '()))
+
