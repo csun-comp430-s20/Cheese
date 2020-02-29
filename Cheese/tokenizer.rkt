be63d2690bf0b283f62c4153fd394886bf127267
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-02-28 19:38:19 -0800

    Add files via upload
    broken

--- Cheese/tokenizer.rkt
+++ Cheese/tokenizer.rkt
@@ -7,9 +7,10 @@
      (if (not (eof-object? character)) (fill_text file (append txt (list character))) txt)))
    
 (define (text file) (fill_text file '()))
+(define characters (text in))
+(define end (length characters))
 
-
-(define (item txt pos) (char->integer (list-ref txt pos)))
+(define (item pos) (char->integer (list-ref characters pos)))
 (define (to_string n) (string (integer->char n)))
 (define (concat val x) (string-append val (to_string x)))
 
@@ -23,62 +24,69 @@
 (define (an_int digits) (string-append "Integer_token(" digits ")"))
 (define (a_string s) (string-append "String_token(" s ")"))
 (define (a_bool b) (string-append "Boolean_token(" (list->string b) ")"))
+(define (an_ident i) (string-append "Identifier_token(" i ")"))
 
 (define (a_token t) (not (void? t)))
 
-(define (Integer_Token txt pos val tokens)
-  (let ([num (item txt pos)])
-    (if (is_int num)
-        (Integer_Token txt (+ pos 1) (concat val num) tokens)
+(define (Integer_Token pos val tokens)
+  (unless (>= pos end)
+    (let ([num (item pos)])
+      (if (is_int num)
+          (Integer_Token (+ pos 1) (concat val num) tokens)
+          (when (> (string-length val) 0)
+            (tokenizer pos (append tokens (list (an_int val)))))))))
+
+(define (String_Token pos val tokens)
+  (unless (>= pos end)
+    (let ([character (item pos)])
+      (unless (is_quotation character)
+        (String_Token (+ pos 1) (concat val character) tokens)
         (when (> (string-length val) 0)
-          (tokenizer txt pos (length txt) (append tokens (list (an_int val))))))))
+          (tokenizer (+ pos 2) (append tokens (list "Quotation_token") (list (a_string val)) (list "Quotation_token"))))))))
 
-(define (String_Token txt pos val tokens)
-  (let ([character (item txt pos)])
-    (unless (is_quotation character)
-        (String_Token txt (+ pos 1) (concat val character) tokens)
-        (when (> (string-length val) 0)
-          (tokenizer txt pos (length txt) (append tokens (list (a_string val)) (list "Quotation_token")))))))
+(define (Boolean_Token pos tokens)
+  (unless (>= pos end)
+    (let ([t (take-right characters (- end pos))])
+      (cond
+        [(list-prefix? (list #\T #\r #\u #\e) t) (tokenizer (+ pos 4) (append tokens (list (a_bool (take t 4)))))]
+        [(list-prefix? (list #\F #\a #\l #\s #\e) t) (tokenizer (+ pos 5) (append tokens (list (a_bool (take t 5)))))]
+        [else (tokenizer pos tokens)]))))
 
-(define (Boolean_Token txt pos val tokens)
-  (let ([t (take-right txt (- (length txt) pos))])
-    (cond
-      [(list-prefix? (list #\T #\r #\u #\e) t) (tokenizer txt (+ pos 4) (length txt) (append tokens (list (a_bool (take t 4)))))]
-      [(list-prefix? (list #\F #\a #\l #\s #\e) t) (tokenizer txt (+ pos 5) (length txt) (append tokens (list (a_bool (take t 5)))))]
-      [else (tokenizer txt pos (length txt) tokens)])))
+(define (LeftParen_Token pos val tokens)
+  (unless (>= pos end)
+    (let ([p (item pos)])
+      (if (is_leftparen p)
+          (LeftParen_Token (add1 pos) (concat val p) tokens)
+          (when (> (string-length val) 0)
+            (tokenizer pos (append tokens (list "LeftParen_token"))))))))
 
-(define (Quotation_Token txt pos val tokens)
-  (let ([q (item txt pos)])
-    (if (is_quotation q)
-        (Quotation_Token txt (+ pos 1) (concat val q) tokens)
-        (when (> (string-length val) 0)
-          (String_Token txt pos "" (append tokens (list "Quotation_token")))))))
+(define (RightParen_Token pos val tokens)
+  (unless (>= pos end)
+    (let ([p (item pos)])
+      (if (is_rightparen p)
+          (RightParen_Token (add1 pos) (concat val p) tokens)
+          (when (> (string-length val) 0)
+            (tokenizer pos (append tokens (list "RightParen_token"))))))))
 
-(define (LeftParen_Token txt pos val tokens)
-  (let ([p (item txt pos)])
-    (if (is_leftparen p)
-        (LeftParen_Token txt (+ pos 1) (concat val p) tokens)
-        (when (> (string-length val) 0)
-          (tokenizer txt pos (length txt) (append tokens (list "LeftParen_token")))))))
+(define (Identifier_Token pos val tokens)
+  (unless (>= pos end)
+    (let ([i (item pos)])
+      (if (is_letter i)
+          (Identifier_Token (add1 pos) (concat val i) tokens)
+          (when (> (string-length val) 0)
+            (tokenizer pos (append tokens (list (an_ident val)))))))))
 
-(define (RightParen_Token txt pos val tokens)
-  (let ([p (item txt pos)])
-    (if (is_rightparen p)
-        (RightParen_Token txt (+ pos 1) (concat val p) tokens)
-        (when (> (string-length val) 0)
-          (tokenizer txt pos (length txt) (append tokens (list "RightParen_token")))))))
-
-(define (tokenizer txt pos end tokens)
-  (if (< pos end)
-      (let ([c (item txt pos)])
+(define (tokenizer pos tokens)
+    (unless (>= pos end)
+      (let ([c (item pos)])
         (cond
-          [(or (= c 10) (= c 32)) (tokenizer txt (+ pos 1) end tokens)]
-          [(is_leftparen c) (LeftParen_Token txt pos "" tokens)]
-          [(is_rightparen c) (RightParen_Token txt pos "" tokens)]
-          [(is_quotation c) (Quotation_Token txt pos "" tokens)]
-          [(is_int c) (Integer_Token txt pos "" tokens)]
-          [else (Boolean_Token txt pos "" tokens)]))
-      tokens))
+          [(or (= c 10) (= c 32)) (tokenizer (add1 pos) tokens)]
+          [(is_quotation c) (String_Token (+ pos 1) "" tokens)]
+          [(or (= c 84) (= c 74)) (Boolean_Token pos tokens)]
+          [(is_leftparen c) (LeftParen_Token pos "" tokens)]
+          [(is_rightparen c) (RightParen_Token pos "" tokens)]
+          [(is_int c) (Integer_Token pos "" tokens)]
+          [else (Identifier_Token pos "" tokens)])))
+  (when (>= pos end) (display tokens)))
 
-(let ([characters (text in)])
-  (tokenizer characters 0 (length characters) '()))
+(tokenizer 0 '())
