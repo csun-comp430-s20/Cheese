daf3f24536e31c19151c3931093e5263e29e1bca
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-03-22 18:47:39 -0700

    Add files via upload
    Parse_function returns a Function consisting of a type, parameters, and a body if the list of tokens is prefixed by the Function pattern described in the concrete grammar else return the pos

--- Cheese/parser.rkt
+++ Cheese/parser.rkt
@@ -3,13 +3,12 @@
 (define amount_of_tokens (length Tokens))
 (define (chop_tokens pos) (take-right Tokens (- amount_of_tokens pos)))
 
-
 (struct If_expression (gaurd ifTrue ifFalse))
 (struct Additive_expression (operand primary1 primary2))
 (struct Multiplicative_expression (operand primary1 primary2))
 (struct Boolean_expression (operand primary1 primary2))
 (struct While_statement (gaurd body))
-(stuct Enum_statement (identifier cases))
+(struct Enum_statement (identifier cases))
 (struct Or_statement (exp1 exp2))
 (struct Switch_statement (exp cases default))
 (struct Call_statement (identifier arguments))
@@ -18,21 +17,20 @@
 (struct String_Expression (value))
 (struct Boolean_Expression (value))
 (struct Variable_Expression (value))
-(struct Function (type identifier parameters))
+(struct Function (type identifier parameters body))
 
 (struct ParseResult (result nextpos))
 
 (define (is_function_expression f)
-  (match f
-    [(list leftparen_Token function_Token type_Token identifier_Token leftparen_Token . rightparen_Token leftcurly_Token . rightcurly_Token _) #t]
-    [(list leftparen_Token function_Token identifier_Token leftparen_Token . rightparen_Token leftcurly_Token . rightcurly_Token) #t]
-    [else #f]))
+  (list-prefix (list leftparen_Token function_Token type_Token identifier_Token leftparen_Token . rightparen_Token leftcurly_Token . rightcurly_Token _ rightparen_Token) f))
 
 (define (Parse_function pos)
   (if (< pos amount_of_tokens)
       (let [f (chopped_tokens pos)]
         (if (is_function_expression f)
-            (ParseResult 
+            (Function_Expression (list-ref f (+ pos 2)) (list-ref f (+ pos 3)) (Parse_Expression (+ pos 5)) (Parse_Expression (+ pos (add1 (index-of f leftcurly_Token)))))
+            (Parse_Expression pos)))
+      pos))
 
       
   
