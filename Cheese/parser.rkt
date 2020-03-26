3684ca9e6729490cd04930c5d545badd91220d9a
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-03-25 20:03:28 -0700

    Add files via upload
    still fixing Parse_function

--- Cheese/parser.rkt
+++ Cheese/parser.rkt
@@ -21,33 +21,35 @@
 
 (struct ParseResult (result nextpos))
 
-(define (is_function_expression f)
-  (list-prefix? (list leftparen_Token function_Token) f))
+(define (is_function_expression pos) (and (leftparen_Token? (list-ref Tokens pos)) (function_Token? (list-ref Tokens (add1 pos)))))
+  ;(list-prefix? (list leftparen_Token function_Token) f))
+
+(is_function_expression 0)
 
 (define (Parse_function pos)
   (if (< pos amount_of_tokens)
-      (if (is_function_expression (chop_tokens pos))
+      (if (is_function_expression pos)
           (let* ([t (ParseResult (list-ref Tokens (+ pos 2)) (+ pos 3))]
             [i (ParseResult (list-ref Tokens (ParseResult-nextpos t)) (add1 (ParseResult-nextpos t)))]
             [p (parse_parameter_decleration (ParseResult-nextpos i))]
             [b (parse_function_body_decleration (ParseResult-nextpos p))]
             [r (Parse_function (ParseResult-nextpos b))])
-            (if (rightparen_Token? (ParseResult-nextpos r))
+            (if (rightparen_Token? (list-ref Tokens (ParseResult-nextpos r)))
                 (ParseResult (Function_Expression i p b r) (add1 ParseResult-nextpos r))
-                (error (concat "invalid syntax, expected: ) but read: " (list-ref Tokens (ParseResult-nextpos r))))))
+                (error "invalid syntax, expected: ) but read: " (list-ref Tokens (ParseResult-nextpos r)))))
             ;(Parse_Expression pos))
-          (display pos))
+          (ParseResult null (add1 pos)))
       pos))
 
 
 (define (parse_parameter_decleration pos)
   (if (leftparen_Token? (list-ref Tokens pos))
       (collect_params (add1 pos) (list))
-      (error (concat "invalid syntax, expected ) but read : " (list-ref Tokens pos)))))
+      (error "invalid syntax, expected ) but read : " (list-ref Tokens pos))))
 
 (define (collect_params pos params)
   (if (rightparen_Token? (list-ref Tokens pos)) (ParseResult params (add1 pos))
-      (let* ([t (check_type_of_param (list-ref Tokens (add1 pos)) pos)]
+      (let* ([t (check_type_of_param (list-ref Tokens pos) pos)]
             [i (check_name_of_param (list-ref Tokens (ParseResult-nextpos t)) (ParseResult-nextpos t))])
         (collect_params (ParseResult-nextpos i)(append params (list (list (list t i))))))))
 
@@ -55,18 +57,18 @@
 (define (check_type_of_param tok pos)
   (if (type_Token? tok)
       (ParseResult (type_Token-value tok) (add1 pos))
-      (error (concat "invalid type given, expected a type int or string but read: " (list-ref Tokens pos)))))
+      (error "invalid type given, expected a type int or string but read: " (list-ref Tokens pos))))
 
 
 (define (check_name_of_param tok pos)
   (if (identifier_Token? tok)
       (ParseResult (Variable_Expression (identifier_Token-value tok)) (add1 pos))
-      (error (concat "expected a variable, read: " (list-ref Tokens pos)))))
+      (error "expected a variable, read: " (list-ref Tokens pos))))
 
 (define (parse_function_body_decleration pos)
   (if (leftcurly_Token? (list-ref Tokens pos))
       (collect_body (add1 pos) (list))
-      (error (concat "invalid syntax, expected: } but read " (list-ref Tokens pos)))))
+      (error "invalid syntax, expected: } but read " (list-ref Tokens pos))))
 
 (define (collect_body pos body)
   (if (rightcurly_Token? (list-ref Tokens pos))
@@ -74,34 +76,7 @@
       (let ([exp_or_stmt (Parse_function pos)])
         (collect_body (ParseResult-nextpos exp_or_stmt) (append body (list exp_or_stmt))))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(Parse_function 0)
 
 
 
