7c5753fa09a041cd39e30ad156c82f8afe4d1382
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-04-12 13:27:46 -0700

    Add files via upload
    FileReader asks for a file, reads the file, generates a list of characters
    tokenizer takes the list of characters and tokenizes them
    parser takes a list of tokens, parses the tokens, returns a list of asts

---
+++ FileReader.rkt
@@ -0,0 +1,14 @@
+#lang racket
+;File opener
+(display "Enter a file to compile: ")
+(define input (read-line (current-input-port)))
+(define in (open-input-file input))
+
+(define (fill_text file txt)
+  (let ([character (read-char file)])
+     (if (not (eof-object? character)) (fill_text file (append txt (list character))) txt)))
+   
+(define (text file) (fill_text file '()))
+(define characters (text in))
+
+(provide characters)
