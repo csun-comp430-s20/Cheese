26b644cd04729f447d31c6fb7ef233026de87397
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-04-27 20:35:24 -0700

    Add files via upload
    Checks boolean expressions

--- typechecker.rkt
+++ typechecker.rkt
@@ -20,4 +20,5 @@
     [(Variable_Expression? exp) (if (hash-has-key? gamma (Variable_Expression-value exp)) (hash-ref (Variable_Expression-value exp)) (error "Variable " (Variable_Expression-value exp) " is out of scope."))]
     [(Additive_Expression? exp) (if (and (Int_Type? (type_of gamma (Additive_Expression-primary1 exp))) (Int_Type? (type_of gamma (Additive_Expression-primary2 exp)))) (Int_Type) (error "one or more expressions is not an int in an additive expression"))]
     [(Multiplicative_Expression? exp) (if (and (Int_Type? (type_of gamma (Multiplicative_Expression-primary1 exp))) (Int_Type? (type_of gamma (Multiplicative_Expression-primary2 exp)))) (Int_Type) (error "one or more expressions is not an int in a multiplicative expression"))]
+    [(Boolean_Operation_Expression? exp) (if (and (Bool_Type? (type_of gamma (Boolean_Operation_Expression-primary1 exp))) (Bool_Type? (type_of gamma (Boolean_Operation_Expression-primary2 exp)))) (Bool_Type) (error "one or more expressions is not a boolean in a boolean operation"))]
     [else (error "unrecognized expression")]))
