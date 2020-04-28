2f9ac56f45b9b40eae884ec6dbba3f2c969989d2
Vel4ta <36457141+Vel4ta@users.noreply.github.com>
2020-04-27 20:30:30 -0700

    Add files via upload
    checks for additive and multiplicative expressions have been added

--- typechecker.rkt
+++ typechecker.rkt
@@ -7,7 +7,7 @@
 (struct String_Type ([hash_code #:auto])
   #:auto-value 1)
 
-(struct Bool_type ([hash_code #:auto])
+(struct Bool_Type ([hash_code #:auto])
   #:auto-value 2)
 
 (define gamma (make-hash))
@@ -16,6 +16,8 @@
   (cond
     [(Integer_Expression? exp) (Int_Type)]
     [(String_Expression? exp) (String_Type)]
-    [(Boolean_Expression? exp) (Boolean_Type)]
+    [(Boolean_Expression? exp) (Bool_Type)]
     [(Variable_Expression? exp) (if (hash-has-key? gamma (Variable_Expression-value exp)) (hash-ref (Variable_Expression-value exp)) (error "Variable " (Variable_Expression-value exp) " is out of scope."))]
+    [(Additive_Expression? exp) (if (and (Int_Type? (type_of gamma (Additive_Expression-primary1 exp))) (Int_Type? (type_of gamma (Additive_Expression-primary2 exp)))) (Int_Type) (error "one or more expressions is not an int in an additive expression"))]
+    [(Multiplicative_Expression? exp) (if (and (Int_Type? (type_of gamma (Multiplicative_Expression-primary1 exp))) (Int_Type? (type_of gamma (Multiplicative_Expression-primary2 exp)))) (Int_Type) (error "one or more expressions is not an int in a multiplicative expression"))]
     [else (error "unrecognized expression")]))
